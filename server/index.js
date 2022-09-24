b55f5aaefa0c2bb807bcb1ccda590f6cda259875
Austin Westbury <austinwestbury@live.com>
2022-09-24 10:54:27 -0400

    testing

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const router = require('./router');
 
 app.use(cors());
 app.use(express.json());
-app.use(router);gi
+app.use(router);
 
 app.listen(5000, () => {
   console.log('server is running on port 5000')
